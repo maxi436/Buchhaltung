fc9547ee28fa208c48dc3d7c98b94d70abf0b772
Maxi <maxi@sanjuan-schroeder.de>
2026-09-09 18:30:54 +0200

    server.py aktualisieren

--- src/server.py
+++ src/server.py
@@ -81,5 +81,9 @@ def eintrag():
             )
     return "true"
 
-if __name__ == "__main__":
-    app.run(port=8000)
+import os
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 8000))
+)
